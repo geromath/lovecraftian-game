c3ad2d62ee14db3149dbb0114085eb33e4c66c4e
Mathias <mathias.maagero@gmail.com>
2022-10-13 23:35:10 +0200

    Messing with gradients for that spooky effect

--- js/components/Sprite.js
+++ js/components/Sprite.js
@@ -1,47 +1,65 @@
 import events from "../utils/events.js";
 import gameArea from "../main.js";
-
+import { boundaries } from "../main.js";
+import rectangularCollision from "../utils/collisionDetection.js";
 class Sprite {
-  constructor (width, height, image, x, y, moveable) {
+  constructor(width, height, image, x, y, moveable) {
     this.width = width;
     this.height = height;
     this.image = image;
-    this.position = {x: x, y: y}
+    this.position = { x: x, y: y };
     this.speed = 5;
     this.moveable = moveable;
   }
 
   draw() {
-    gameArea.context.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    gameArea.context.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
+
+  checkForCollision(direction) {
+    for (let i = 0; i < boundaries.length; i++) {
+      if (
+        rectangularCollision(
+          {
+            ...this,
+            position: {
+              x: this.position.x + this.speed * direction.x,
+              y: this.position.y + this.speed * direction.y,
+            },
+          },
+          boundaries[i]
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
   }
 
   movement() {
-    if(events.a) {
-      
-      if(this.position.x - this.speed < 0) {
-        this.position.x = 0
-      } else {
+    if (events.a) {
+      if (!this.checkForCollision({ x: -1, y: 0 })) {
         this.position.x -= this.speed;
       }
     }
-    if (events.d){
-      if(this.position.x + this.speed + this.width > gameArea.width) {
-        this.position.x =gameArea.width - this.width;
-      } else {
+    if (events.d) {
+      if (!this.checkForCollision({ x: 1, y: 0 })) {
         this.position.x += this.speed;
       }
     }
-    if(events.s) {
-      if(this.position.y + this.speed + this.height > gameArea.height) {
-        this.position.y = gameArea.height - this.height;
-      } else {
+    if (events.s) {
+      if (!this.checkForCollision({ x: 0, y: 1 })) {
         this.position.y += this.speed;
       }
     }
-    if (events.w){
-      if(this.position.y - this.speed < 0) {
-        this.position.y = 0;
-      } else {
+    if (events.w) {
+      if (!this.checkForCollision({ x: 0, y: -1 })) {
         this.position.y -= this.speed;
       }
     }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 import Boundary from "./components/Boundary.js";
 import Sprite from "./components/Sprite.js";
-import events from "./utils/events.js"
-import collisions from "../assets/collisions.js"
+import events from "./utils/events.js";
+import collisions from "../assets/collisions.js";
 import rectangularCollision from "./utils/collisionDetection.js";
-
 
 // Game Area singleton
 let gameArea = {
-  canvas: document.getElementById('canvas'),
-  context: canvas.getContext('2d'),
+  canvas: document.getElementById("canvas"),
+  context: canvas.getContext("2d"),
   width: 640,
   height: 512,
   x: 0,
@@ -18,18 +17,18 @@ let gameArea = {
   start: function () {
     this.canvas.width = this.width;
     this.canvas.height = this.height;
-    this.interval = setInterval(update, 1000/50);
+    this.interval = setInterval(update, 1000 / 50);
     this.context.imageSmoothingEnabled = false;
   },
   update: function () {
     this.context.clearRect(0, 0, this.width, this.height);
     background.update();
-  }
-}
+  },
+};
 
 // Map collisions json to a 2d map of arrays
 const collisionsMap = [];
-for(let i = 0; i < collisions.length; i += 40) {
+for (let i = 0; i < collisions.length; i += 40) {
   collisionsMap.push(collisions.slice(i, i + 40));
 }
 
@@ -37,38 +36,81 @@ for(let i = 0; i < collisions.length; i += 40) {
 const boundaries = [];
 collisionsMap.forEach((row, i) => {
   row.forEach((tile, j) => {
-    tile !== 0 && boundaries.push(new Boundary({position: {x: j * gameArea.tileSize * gameArea.scale, y: i * gameArea.tileSize * gameArea.scale}, width: 16 * gameArea.scale, height: 16 * gameArea.scale}))
-  })
-})
+    tile !== 0 &&
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * gameArea.tileSize * gameArea.scale,
+            y: i * gameArea.tileSize * gameArea.scale,
+          },
+          width: 16 * gameArea.scale,
+          height: 16 * gameArea.scale,
+        })
+      );
+  });
+});
 
 /*  
 Set up the background
 Should be done in a separate file maybe to handle level loading?
 */
 const bg = new Image();
-bg.src = '../assets/map.png';
-const background = new Sprite(gameArea.width*gameArea.scale, gameArea.scale*gameArea.height, bg, 0, 0, false)
+bg.src = "../assets/map.png";
+const background = new Sprite(
+  gameArea.width * gameArea.scale,
+  gameArea.scale * gameArea.height,
+  bg,
+  0,
+  0,
+  false
+);
 
 /* Set up the player sprite/character */
 // TODO: refactor to use a Player class that inherits from Sprite
 const playerImage = new Image();
-playerImage.src = '../assets/char.png';
-const player = new Sprite(16*gameArea.scale, 32*gameArea.scale, playerImage, 200, 200, true);
+playerImage.src = "../assets/char.png";
+const player = new Sprite(
+  16 * gameArea.scale,
+  32 * gameArea.scale,
+  playerImage,
+  100,
+  100,
+  true
+);
+
+// Should be moved later
+const grad = {
+  update: function () {
+    let grd = gameArea.context.createRadialGradient(
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2,
+      1,
+      player.position.x + player.width / 2,
+      player.position.y + player.height / 2,
+      300
+    );
+    grd.addColorStop(0, "transparent");
+    grd.addColorStop(0.8, "rgba(0, 0, 0, .4)");
+    gameArea.context.fillStyle = grd;
+    gameArea.context.fillRect(0, 0, gameArea.width, gameArea.height);
+  },
+};
 
 const update = () => {
   gameArea.update();
   player.update();
-}
+  grad.update();
+};
 
 // Initiate the startup
 const start = () => {
-  console.log('Starting');
+  console.log("Starting");
   events.start();
   gameArea.start();
-  boundaries.push(new Boundary({position: {x: 4* gameArea.tileSize * gameArea.scale, y: 4* gameArea.tileSize * gameArea.scale}, width: 16 * gameArea.scale, height: 16 * gameArea.scale}))
-}
+};
 
 // Start the game!
 start();
 
 export default gameArea;
+export { boundaries };
